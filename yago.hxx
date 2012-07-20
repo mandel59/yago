df6eed7091fd0a1dd4b2b9ca9060583fbc53ee0c
Ryusei Yamaguchi <mandel59@gmail.com>
2012-07-21 08:07:35 +0900

    use std::function

--- yago.hxx
+++ yago.hxx
@@ -2,6 +2,7 @@
 #define YAGO_H
 
 #include <cmath>
+#include <functional>
 #include <list>
 #include <vector>
 #include <utility>
@@ -24,27 +25,27 @@ public:
 
 class Oscillator : SignalElement<yago::sample>
 {
-    typedef yago::sample wave_func(float phase);
-    typedef float freq_func(yago::time t);
+    typedef std::function<float (float)> wave_func;
+    typedef std::function<float (yago::time)> freq_func;
 private:
     float _frequency;
-    freq_func * _freq_f;
+    freq_func _freq_f;
     float _phase;
-    wave_func * _wave;
+    wave_func _wave;
 public:
     static yago::sample sine_wave(float phase)
     {
         return sin(twopi * phase);
     }
-    Oscillator(const float frequency, const float phase = 0.0f, const wave_func * wave = sine_wave) : _frequency(frequency), _freq_f(nullptr), _phase(phase - (int) phase), _wave(wave) {}
-    Oscillator(const freq_func * frequency, const float phase = 0.0f, const wave_func * wave = sine_wave) : _frequency(0.0), _freq_f(frequency), _phase(phase - (int) phase), _wave(wave) {}
+    Oscillator(const float frequency, const float phase = 0.0f, const wave_func wave = sine_wave) : _frequency(frequency), _freq_f(nullptr), _phase(phase - (int) phase), _wave(wave) {}
+    Oscillator(const freq_func frequency, const float phase = 0.0f, const wave_func wave = sine_wave) : _frequency(0.0), _freq_f(frequency), _phase(phase - (int) phase), _wave(wave) {}
     void signal(const yago::time t, const int sample_rate, std::vector<yago::sample>& buffer);
     void set_frequency(const float frequency)
     {
         _frequency = frequency;
         _freq_f = nullptr;
     }
-    void set_frequency(const freq_func * frequency)
+    void set_frequency(const freq_func frequency)
     {
         _freq_f = frequency;
     }
